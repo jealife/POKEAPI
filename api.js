71f3a615d38d2711041f5b796eec46f752d54922
Jealife <jeanguylanememiaghe@gmail.com>
2024-10-22 10:07:30 +0100

    Changes to be committed: 	modified:   api.js 	modified:   index.html

--- api.js
+++ api.js
@@ -2,6 +2,27 @@ let image = document.getElementById("image");
 let PokeNumber = document.getElementById("number");
 let PokeName = document.getElementById("name");
 let button = document.getElementById("button");
+var audio = document.getElementById("audio");
+      // Permet de préfixer les navigateurs
+navigator.vibrate =
+navigator.vibrate ||
+navigator.webkitVibrate ||
+navigator.mozVibrate ||
+navigator.msVibrate;
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+function copy() {
+    navigator.clipboard.writeText("jealife.agency@gmail.com");
+    btnCopy.innerText = "Copied!!";
+        // Si l'API vibrate est supportée, on fait vibrer le téléphone 1/2 seconde
+        if (navigator.vibrate) {
+
+        navigator.vibrate(500);
+}
+play();
+}
 
 button.addEventListener("click",  changePokemon  = async ()=>{
     let RandomNumber= Math.ceil(Math.random()*150)+1;
@@ -14,5 +35,6 @@ button.addEventListener("click",  changePokemon  = async ()=>{
     image.src=response.sprites.front_default;
     PokeNumber.textContent=response.id;
     PokeName.textContent=response.name;
+    play();
 })
 changePokemon();
